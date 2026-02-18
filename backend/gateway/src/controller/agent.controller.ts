82df6935059262abea0608bef730fa77415c546a
Prashantt6 <royprashant14@gmail.com>
2026-02-18 18:43:13 +0545

    Gateway to Service authentication done

--- backend/gateway/src/controller/agent.controller.ts
+++ backend/gateway/src/controller/agent.controller.ts
@@ -3,13 +3,16 @@ import getIntent from "../services/aiClient";
 import { Request, Response } from "express";
 import todoService from "../services/todoClient";
 
+
 const getIntentController = async (req: Request, res: Response)=>{
     const {input} = req.body
     const intent =await getIntent(input)
+    const header = req.headers.authorization
+    // console.log(header)
 
     // console.log(`This api was hit by ${req.user?.username}`)
     if(intent === 'add_task' || intent==='delete_task' || intent==='list_tasks'){
-        todoService(input, intent)
+        todoService(input, intent,header)
     }
     else if(intent === 'write_blog' ){
         
