f67599009198998f5f73fc98b7426f4e50cf2b10
Prashantt6 <royprashant14@gmail.com>
2026-02-19 15:12:00 +0545

    Handled the getIntent axios failure

--- backend/gateway/src/services/aiClient.ts
+++ backend/gateway/src/services/aiClient.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { get } from "node:http";
 
 const getIntent = async(input: string) =>{
+    try{
     const response = await axios.post("http://127.0.0.1:8000/get_intent",{
         text: input
         
@@ -12,7 +13,13 @@ const getIntent = async(input: string) =>{
     
     // console.log(intent)
     return intent
-
+    }
+    catch(error){
+        console.error(error)
+        console.log("Error in calling aiService axios")
+        return (
+            "Error in calling aiService axios")
+    }
 }
 // getIntent("Yo kaam garde")
 
